beef6f5422f0f6284297ae2fd48b3519414d4ead
shivang <1616510104@kit.ac.in>
2021-05-12 00:07:13 +0530

    Commit Changes

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app.secret_key = "OTP"
 @app.route("/")
 def index():
     return "Hello World"
-@app.route("/getotp",methods=["POST"])
+@app.route("/getotp",methods=["POST","GET"])
 def getotp():
     number = request.form["number"]
-    print("Number = ",number)
+    # print("Number = ",number)
     print(type(number))
     val = getotpAPi(number)
     if val == "True":
@@ -31,7 +31,7 @@ def validate():
 def generateOTP():
     return random.randrange(1000,9999)
 def getotpAPi(number):
-    print("Receive Number = ",number)
+    # print("Receive Number = ",number)
     acc_sid = settings.ACC_SID
     auth_token = settings.ACC_AUTH_TOKEN
     client = Client(acc_sid,auth_token)
